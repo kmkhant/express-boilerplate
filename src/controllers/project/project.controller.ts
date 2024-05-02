35f0e9ee826a3aaa1863afbe652b0753ef3a46eb
Khaing Myel Khant <khaingmyelkhant@ucsm.edu.mm>
2024-05-02 22:27:39 +0630

    add: project routes and handlers

--- src/controllers/project/project.controller.ts
+++ src/controllers/project/project.controller.ts
@@ -2,6 +2,10 @@ import { Request, Response } from "express";
 import ProjectModel from "@/models/project.model";
 import AdminModel from "@/models/admin.model";
 import { secureHash } from "@/utils/secureHash";
+import ChannelModel from "@/models/channel.model";
+
+// import types
+import { IUserInfo } from "@/types/";
 
 export const getProjectsByAdmin = async (
 	req: Request,
@@ -23,7 +27,15 @@ export const createProject = async (
 	res: Response
 ) => {
 	try {
-		const { name, description, userInfo } = req.body;
+		const {
+			name,
+			description,
+			userInfo,
+		}: {
+			name: string;
+			description: string;
+			userInfo: IUserInfo;
+		} = req.body;
 
 		if (!name || !description || !userInfo) {
 			return res
@@ -75,6 +87,12 @@ export const updateProject = async (
 	const { id } = req.params;
 	const { name, description, userInfo } = req.body;
 
+	if (!userInfo) {
+		return res
+			.status(401)
+			.json({ message: "Unauthorized" });
+	}
+
 	if (!name || !description) {
 		return res
 			.status(400)
@@ -108,4 +126,228 @@ export const updateProject = async (
 	}
 
 	return res.status(200).json({ message: "OK" });
+};
+
+// add channel to project
+export const addChannelToProject = async (
+	req: Request,
+	res: Response
+) => {
+	const { id } = req.params;
+	const {
+		channelId,
+		channelName,
+		channelDescription,
+		userInfo,
+	} = req.body;
+
+	if (!userInfo) {
+		return res
+			.status(401)
+			.json({ message: "Unauthorized" });
+	}
+
+	if (!channelId || !id || !channelName) {
+		return res
+			.status(400)
+			.json({ message: "Invalid Request" });
+	}
+
+	const currentProject = await ProjectModel.findOne({
+		_id: id,
+	}).populate("admin");
+
+	if (
+		currentProject &&
+		currentProject.admin instanceof AdminModel &&
+		currentProject.admin.id !== secureHash(userInfo.id)
+	) {
+		return res.status(403).json({ message: "Forbidden" });
+	}
+
+	if (!currentProject) {
+		return res
+			.status(404)
+			.json({ message: "Project not found" });
+	}
+
+	const secureChannelId = secureHash(channelId);
+
+	const currentChannel = await ChannelModel.findOne({
+		id: secureChannelId,
+	});
+
+	if (currentChannel && currentChannel._id) {
+		// check if channel already exists in the project
+		const channelExists = currentProject.channels.some(
+			(channel) => channel.equals(currentChannel._id)
+		);
+
+		console.log(channelExists);
+
+		if (channelExists)
+			return res
+				.status(400)
+				.json({ message: "Channel already exists." });
+	}
+
+	if (!currentChannel) {
+		const newChannel = await ChannelModel.create({
+			id: secureChannelId,
+			name: channelName,
+			description: channelDescription,
+		});
+
+		await ProjectModel.findOneAndUpdate(
+			{
+				_id: id,
+			},
+			{
+				$push: { channels: newChannel },
+			}
+		);
+
+		return res.status(200).json({ message: "OK" });
+	}
+
+	return res
+		.status(500)
+		.json({ message: "Internal Server Error" });
+};
+
+export const deleteChannelFromProject = async (
+	req: Request,
+	res: Response
+) => {
+	const { projectId, channelId } = req.params; // project id
+	const { userInfo } = req.body;
+
+	if (!userInfo) {
+		return res
+			.status(401)
+			.json({ message: "Unauthorized" });
+	}
+
+	if (!channelId || !projectId) {
+		return res
+			.status(400)
+			.json({ message: "Invalid Request" });
+	}
+
+	const currentAdmin = await AdminModel.findOne({
+		id: secureHash(userInfo.id),
+	});
+
+	if (!currentAdmin) {
+		return res.status(403).json({ message: "Forbidden" });
+	}
+
+	const currentProject = await ProjectModel.findOne({
+		_id: projectId,
+	}).populate("admin");
+
+	if (!currentProject) {
+		return res
+			.status(404)
+			.json({ message: "Project not found" });
+	}
+
+	const secureAdminId = secureHash(userInfo.id);
+	const secureChannelId = channelId;
+
+	// check if the user is the admin of the project
+	if (
+		currentProject &&
+		currentProject.admin instanceof AdminModel &&
+		currentAdmin.id !== secureAdminId
+	) {
+		return res.status(403).json({ message: "Forbidden" });
+	}
+
+	const currentChannel = await ChannelModel.findOne({
+		id: secureChannelId,
+	});
+
+	if (!currentChannel) {
+		return res
+			.status(404)
+			.json({ message: "Channel not found" });
+	}
+
+	// check if channel exists in the project
+	const channelExists = currentProject.channels.some(
+		(channel) => channel.equals(currentChannel._id)
+	);
+
+	if (channelExists) {
+		await ProjectModel.findOneAndUpdate(
+			{
+				_id: projectId,
+			},
+			{
+				$pull: { channels: currentChannel._id },
+			}
+		);
+
+		await ChannelModel.findOneAndDelete({
+			id: secureChannelId,
+		});
+
+		return res
+			.status(200)
+			.json({ message: "deleted channel" });
+	} else {
+		return res
+			.status(404)
+			.json({ message: "Channel is not in the project" });
+	}
+};
+
+// delete project
+export const deleteProject = async (
+	req: Request,
+	res: Response
+) => {
+	const projectId = req.params.projectId;
+
+	const { userInfo }: { userInfo: IUserInfo } = req.body;
+
+	if (!projectId || !userInfo) {
+		return res
+			.status(400)
+			.json({ message: "Invalid Request" });
+	}
+
+	const currentAdmin = await AdminModel.findOne({
+		id: secureHash(userInfo.id),
+	});
+
+	if (!currentAdmin) {
+		return res.status(403).json({ message: "Forbidden" });
+	}
+
+	const currentProject = await ProjectModel.findOne({
+		_id: projectId,
+	}).populate("admin");
+
+	if (!currentProject) {
+		return res
+			.status(404)
+			.json({ message: "Project not found" });
+	}
+
+	if (
+		currentProject &&
+		currentProject.admin instanceof AdminModel &&
+		currentAdmin.id !== secureHash(userInfo.id)
+	) {
+		return res.status(403).json({ message: "Forbidden" });
+	} else {
+		await ProjectModel.findOneAndDelete({
+			_id: projectId,
+		});
+		return res
+			.status(200)
+			.json({ message: "OK, deleted the project" });
+	}
 };
--- src/controllers/project/project.routes.ts
+++ src/controllers/project/project.routes.ts
@@ -4,12 +4,24 @@ import {
 	createProject,
 	getProjectsByAdmin,
 	updateProject,
+	addChannelToProject,
+	deleteChannelFromProject,
+	deleteProject,
 } from "./project.controller";
 
 const router = express.Router();
 
+// projects CRUD
+router.post("/create", createProject);
 router.get("/", getProjectsByAdmin);
-router.post("/", createProject);
-router.put("/:id", updateProject);
+router.put("/update/:id", updateProject);
+router.delete("/delete/:projectId", deleteProject);
+
+// channel CRUD
+router.post("/addChannel/:id", addChannelToProject);
+router.delete(
+	"/deleteChannel/:projectId/:channelId",
+	deleteChannelFromProject
+);
 
 export default router;
